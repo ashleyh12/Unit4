60132afec53aff97464fd1f878bb08783487f66c
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2022-09-18 11:13:47 -0400

    changes

--- app/src/main/java/com/example/unit3/MainActivity.kt
+++ app/src/main/java/com/example/unit3/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.unit3
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
 
+private const val now_playing = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1"
 class MainActivity : AppCompatActivity() {
+
+
+    private val movies = mutableListOf<Movie>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val asyncClient = AsyncHttpClient()
+        asyncClient.get(now_playing, object: JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode")
+
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                Log.i(TAG, "onSuccess: JSON data $json")
+                try {
+
+
+                    val jsonArray = json!!.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(jsonArray))
+                    Log.i(TAG, "List of Movies $movies ")
+                } catch (e: JSONException) {
+                    Log.e(TAG, "Encountered Exception $e")
+                }
+
+            }
+
+        })
+
     }
 }
---
+++ app/src/main/java/com/example/unit3/Movie.kt
@@ -0,0 +1,31 @@
+package com.example.unit3
+
+import org.json.JSONArray
+
+data class Movie(
+    val movieID: Int,
+    private val poster: String,
+    val title: String,
+    val description: String,
+
+
+) {
+    val posterURL = "https://image.tmdb.org/t/p/w500/$poster"
+    companion object {
+        fun fromJsonArray(jsonArray: JSONArray): List<Movie> {
+            val movies = mutableListOf<Movie>()
+            for (i in 0 until jsonArray.length()) {
+                val movieJSON = jsonArray.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        movieJSON.getInt("id"),
+                        movieJSON.getString("poster"),
+                        movieJSON.getString("title"),
+                        movieJSON.getString("description"),
+                    )
+                )
+            }
+            return movies
+        }
+    }
+}
