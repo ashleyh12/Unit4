f7271a2b5c0674ad3c382a9e79bde728f518e907
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2022-09-26 11:52:04 -0400

    commit

--- app/src/main/java/com/example/unit3/MainActivity.kt
+++ app/src/main/java/com/example/unit3/MainActivity.kt
@@ -14,7 +14,7 @@ import org.json.JSONException
 
 //source(s) used: https://www.youtube.com/watch?v=mPlcAkuiH5M&list=PLrT2tZ9JRrf4wZSRwvvl-0eYqLCsP0_7q&ab_channel=CodePath
 private const val newTag = "mainActivity Checker"
-private const val now_playing = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1"
+private const val now_playing = "https://api.themoviedb.org/3/movie/now_playing?api_key" + "=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1"
 class MainActivity : AppCompatActivity() {
 
 
